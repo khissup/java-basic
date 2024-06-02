e9ed6f45f96f3406ff85ca483442a033834175f2
khissup <khissup@naver.com>
2024-06-02 23:25:07 +0900

    Update DoubleTest.java

--- src/DoubleTest.java
+++ src/DoubleTest.java
@@ -4,6 +4,6 @@ public class DoubleTest {
         float fnum = 3.14f;
         // 실수리터럴은 기본적으로 double형으로 저장되므로, float에 저장시 뒤에 f붙여서 float형(4byte)로 저장해야함.
         System.out.println(dnum);
-        System.out.println(fnum);
+        System.out.println(fnum);//
     }
 }
